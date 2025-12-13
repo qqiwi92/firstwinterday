3e86b2cc735f5fb3818cfffb79e7bc847821079b
qiwi <qiwi@qiwi.qiwi>
2025-12-13 09:58:04 +0300

    add rectangle

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
-#include <iostream>
 #include <cmath>
+#include <iostream>
 namespace top
 {
 struct p_t
@@ -46,7 +46,6 @@ struct VerticalLine : IDraw
   size_t len;
 };
 
-
 struct DiagonalLine : IDraw
 {
   DiagonalLine(p_t left_bottom, size_t len);
@@ -54,6 +53,14 @@ struct DiagonalLine : IDraw
   p_t next(p_t) const override;
   p_t left_bottom;
   size_t len; // not true
+};
+
+struct Rectangle : IDraw
+{
+  Rectangle(p_t left_bottom, p_t right_top);
+  p_t begin() const override;
+  p_t next(p_t) const override;
+  p_t left_bottom, right_top;
 };
 
 size_t points(const IDraw &d, p_t **pts, size_t s);
@@ -106,33 +113,62 @@ top::p_t top::VerticalLine::next(p_t prev) const
   return {prev.x, prev.y - 1};
 }
 
-
 top::p_t top::DiagonalLine::begin() const { return left_bottom; }
 
 top::p_t top::DiagonalLine::next(p_t prev) const
 {
-    if (prev.x - left_bottom.x + 1 >= static_cast<int>(len))
+  if (prev.x - left_bottom.x + 1 >= static_cast<int>(len))
   {
     return left_bottom;
   }
 
-  return {prev.x + 1, prev.y +1 };
+  return {prev.x + 1, prev.y + 1};
 }
 
+top::p_t top::Rectangle::begin() const { return {left_bottom.x, right_top.y}; }
+
+top::p_t top::Rectangle::next(p_t prev) const
+{
+  if (prev.y == right_top.y && prev.x < right_top.x)
+  {
+    return {prev.x + 1, prev.y};
+  }
+  else if (prev.x == right_top.x && prev.y > left_bottom.y)
+  {
+    return {prev.x, prev.y - 1};
+  }
+  else if (prev.y == left_bottom.y && prev.x > left_bottom.x)
+  {
+    return {prev.x - 1, prev.y};
+  }
+  else if (prev.x == left_bottom.x && prev.y < right_top.y)
+  {
+    return {prev.x, prev.y + 1};
+  }
+  else
+  {
+    return begin();
+  }
+}
 
 bool top::operator==(p_t a, p_t b) { return a.x == b.x && a.y == b.y; }
 bool top::operator!=(p_t a, p_t b) { return !(a == b); }
 top::HorizontalLine::HorizontalLine(top::p_t left_end, size_t len)
     : IDraw(), left_end{left_end}, len{len}
-{}
+{
+}
 
 top::VerticalLine::VerticalLine(top::p_t top_end, size_t len)
     : IDraw(), top_end{top_end}, len{len}
-{}
+{
+}
 
-top::DiagonalLine::DiagonalLine(p_t s, size_t l)
-    : left_bottom{s}, len{l} {}
+top::DiagonalLine::DiagonalLine(p_t s, size_t l) : left_bottom{s}, len{l} {}
 
+top::Rectangle::Rectangle(p_t left_bottom, p_t right_top)
+    : left_bottom(left_bottom), right_top(right_top)
+{
+}
 
 void top::extend(p_t **pts, size_t s, p_t p)
 {
@@ -218,24 +254,25 @@ void top::flush(std::ostream &os, const char *cnv, f_t fr)
 
 int main()
 {
+  using top::DiagonalLine;
   using top::Dot;
   using top::f_t;
   using top::HorizontalLine;
   using top::IDraw;
   using top::p_t;
+  using top::Rectangle;
   using top::VerticalLine;
-  using top::DiagonalLine;
   size_t error = 0;
   IDraw *shps[100] = {};
   p_t *pts = nullptr;
   size_t s = 0;
   try
   {
-    shps[0] = new DiagonalLine({0,-2}, 9);
+    shps[0] = new Rectangle({0, -2}, {10, 4});
     shps[1] = new HorizontalLine({4, 4}, 2);
     shps[2] = new Dot(-10, -2);
     shps[3] = new VerticalLine({-1, -3}, 3);
-    for (size_t i = 0; i < 4; ++i)
+    for (size_t i = 0; i < 1; ++i)
     {
       s += points(*(shps[i]), &pts, s);
     }
