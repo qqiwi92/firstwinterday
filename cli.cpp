db1fece2c7a1d3586b8a520fc2b89a5043089801
qiwi <qiwi@qiwi.qiwi>
2025-12-25 15:22:01 +0300

    add: add funcs

--- cli.cpp
+++ cli.cpp
@@ -1,12 +1,22 @@
+#include <cctype>
 #include <iostream>
+#include <istream>
+#include <ostream>
 
 void hi() { std::cout << "<HI!>\n"; }
+size_t match(const char * word, const char* const * words, size_t k);
+std::istream getword(std::istream&is, char* word, size_t k, bool(*c)(char));
+bool is_space(char c) {
+    return std::isspace(c);
+}
 int main()
 {
-  constexpr size_t cmds_cout = 1;
-  void (*cmds[cmds_cout])() = {hi};
-  size_t i = 0;
-  while (!(std::cin >> i).eof())
+  constexpr size_t cmds_count = 1;
+  void (*cmds[cmds_count])() = {hi};
+  const char * const cmds_text[] = {"hi"};
+  constexpr size_t bsize = 255;
+  char word[bsize+1] = {};
+  while (!(getword(std::cin, word, bsize, is_space).eof()))
   {
     if (std::cin.fail())
     {
@@ -14,7 +24,7 @@ int main()
       return 1; // нужно масками вернуть норм инпут чтобы цикла не было
                 // бесконенчного
     }
-    else if (i < cmds_cout)
+    else if (size_t i = match(word, cmds_text, cmds_count); i < cmds_count)
     {
       cmds[i]();
     }
